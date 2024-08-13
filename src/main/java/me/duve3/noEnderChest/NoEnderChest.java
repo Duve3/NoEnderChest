904f9869719777b420c72fe37f0e4d3474ef4181
Duve3 <Duv3tabest@gmail.com>
2024-08-12 18:33:24 -0700

    working version

--- src/main/java/me/duve3/noEnderChest/NoEnderChest.java
+++ src/main/java/me/duve3/noEnderChest/NoEnderChest.java
@@ -1,5 +1,6 @@
 package me.duve3.noEnderChest;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Player;
@@ -8,18 +9,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO: can bypass by using number keys, and using shulkerboxes, also prevents you from moving it in yr inv (might force dropping)
-
 public final class NoEnderChest extends JavaPlugin implements Listener {
 
-    String CancelledMessage = "[NoEnderChest] You cannot put this item in an Ender chest!";
-    String CancelledShulker = "[NoEnderChest] This shulkerbox contains a banned item!";
-    List<Material> bannedItems = new ArrayList<Material>();
+    String CancelledMessage = ChatColor.RED + "[NoEnderChest] You cannot put this item in an Ender chest!";
+    String CancelledShulker = ChatColor.RED + "[NoEnderChest] This shulker box contains a banned item!";
+    List<Material> bannedItems = new ArrayList<>();
 
     @Override
     public void onEnable() {
@@ -36,10 +36,24 @@ public final class NoEnderChest extends JavaPlugin implements Listener {
         // Plugin shutdown logic
     }
 
+    private boolean isShulkerBox(Material m) {
+        return switch (m) {
+            case LIGHT_GRAY_SHULKER_BOX, BLACK_SHULKER_BOX, BLUE_SHULKER_BOX, BROWN_SHULKER_BOX, CYAN_SHULKER_BOX,
+                 GRAY_SHULKER_BOX, GREEN_SHULKER_BOX, LIGHT_BLUE_SHULKER_BOX, LIME_SHULKER_BOX, MAGENTA_SHULKER_BOX,
+                 ORANGE_SHULKER_BOX, PINK_SHULKER_BOX, PURPLE_SHULKER_BOX, RED_SHULKER_BOX, WHITE_SHULKER_BOX,
+                 YELLOW_SHULKER_BOX, SHULKER_BOX -> true;
+            default -> false;
+        };
+    }
+
     private void shulkerBoxCheck(Material itemMoved, Material item, InventoryClickEvent e, Player player) {
-        if (itemMoved == Material.SHULKER_BOX) {
-            ShulkerBox shulker = (ShulkerBox) itemMoved.createBlockData();
+        if (isShulkerBox(itemMoved)) {
+            ShulkerBox shulker = (ShulkerBox) ((BlockStateMeta) e.getCurrentItem().getItemMeta()).getBlockState();
             for (ItemStack si : shulker.getInventory().getContents()) {
+                if (si == null) {
+                    continue;
+                }
+
                 if (si.getType() == item) {
                     this.getLogger().info("Cancelling event because of SHULKER_BOX_CHECK");
                     e.setCancelled(true);
@@ -49,7 +63,7 @@ public final class NoEnderChest extends JavaPlugin implements Listener {
 
         }
     }
-    // WE IGNORE INVENTORY DRAG EVENT BECAUSE WE ARE CALCULATING FOR NONSTACKABLES
+    // WE IGNORE INVENTORY DRAG EVENT <- I tested drag event, it isn't necessary anyway.
 
     @EventHandler()
     public void onItemMove(InventoryClickEvent e) {
